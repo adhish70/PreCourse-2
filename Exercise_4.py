76742351866c42454e655e56e35367b2d3181b6b
Adhish Shrivastava <adhish70@gmail.com>
2021-12-13 09:53:28 -0500

    Merge Sort done

--- Exercise_4.py
+++ Exercise_4.py
@@ -1,16 +1,47 @@
 # Python program for implementation of MergeSort 
 def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+
+        left = mergeSort(arr[:mid])
+        right = mergeSort(arr[mid:])
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+        
+    return arr
+
   
   #write your code here
   
 # Code to print the list 
 def printList(arr): 
-    
+    print(arr)
     #write your code here
   
 # driver code to test the above code 
 if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
+    arr = [12, 11, 13, 5, 6]  
     print ("Given array is", end="\n")  
     printList(arr) 
     mergeSort(arr) 
